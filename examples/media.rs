46a601a4dd2d93b805419e67854defa7ba4d9506
Marijn Suijten <marijns95@gmail.com>
2021-09-17 01:24:55 +0200

    examples/media: Demonstrate how to query media devices and links

---
+++ examples/media.rs
@@ -0,0 +1,15 @@
+use std::io;
+
+use v4l::media::Device;
+
+fn main() -> io::Result<()> {
+    let dev = Device::new(0)?;
+    dbg!(dev.device_info()?);
+    for e in dev.enum_entities()? {
+        dbg!(&e.name, e.device_path()?, dev.enum_links(&e)?);
+        if let Some(d) = e.device()? {
+            dbg!(d.query_caps()?);
+        }
+    }
+    Ok(())
+}
